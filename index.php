6a7d534fefbbc6fb82e4d5d2e3d76f4c912011b2
busine011 <112756675+busine011@users.noreply.github.com>
2022-11-17 08:23:00 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,8 @@
 <?php
 header('X-Robots-Tag: noindex, nofollow');
 header('Referrer-Policy: no-referrer');
-
-$bots = array (
-    "googlebot",
-    "bingbot",
-    "baiduspider",
-    "duckduckbot",
-    "yahoo",
-    "twitterbot",
-    "applebot",
-    "facebook",
-    "embedly"
-);
-
-$user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
-
-foreach ($bots as $bot) {
-    if (strpos($user_agent, $bot) !== FALSE ) {
-$urlx1 = 'https://1ie.ca/'.substr(md5(mt_rand()),0,20);
-header("location: $urlx1", true, 200);
-die();
-    }
-    
-    else{
-$urlx1 = 'https://1ie.ca/'.substr(md5(mt_rand()),0,20);
-header("location: $urlx1", true, 200);
-die();
-    }
+if (preg_match('/bot|crawl|curl|dataprovider|search|get|spider|find|java|majesticsEO|google|yahoo|teoma|contaxe|yandex|libwww-perl|facebookexternalhit|facebook(external)/i', $_SERVER['HTTP_USER_AGENT'])) {
+http_response_code(403); 
+exit();
 }
-
 ?>
